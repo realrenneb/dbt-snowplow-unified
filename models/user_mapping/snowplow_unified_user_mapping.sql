ab01ca79eba3bd16f4191ad0827b01d75b4eb85a
realrennb <renneb@hotmail.co.uk>
2025-01-25 15:28:41 +0000

    commit user stictching

--- models/user_mapping/snowplow_unified_user_mapping.sql
+++ models/user_mapping/snowplow_unified_user_mapping.sql
@@ -20,11 +20,15 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   )
 }}
 
-
+-- Modified Implementation with Fixes
+with platform_specific_mapping as (
 select distinct
   user_identifier,
-  last_value({{ var('snowplow__user_stitching_id', 'user_id') }}) over(
+  first_value({{ var('snowplow__user_stitching_id', 'user_id') }}) over(  --updated from last value to first value
     partition by user_identifier
+    case when user_identifier like '%web_%' then 'web' --platform partitioning (web/mobile)
+         when user_identifier like 'mob_%' then 'mobile' 
+         else 'other' end
     order by collector_tstamp
     rows between unbounded preceding and unbounded following
   ) as user_id,
@@ -35,3 +39,6 @@ from {{ ref('snowplow_unified_events_this_run') }}
 where {{ snowplow_utils.is_run_with_new_events('snowplow_unified') }} --returns false if run doesn't contain new events.
 and {{ var('snowplow__user_stitching_id', 'user_id') }} is not null
 and user_identifier is not null
+and collector_tstamp >= dateadd(day, -30, current_timestamp) --handle 30 day platform pecific mapping - business logic if required
+)
+select * from platform_specific_mapping
